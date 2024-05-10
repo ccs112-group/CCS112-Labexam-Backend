abd19dbbb63a96ff5a8aa6c042c64050f2ab2aea
JaramilloChristian28 <jaramillochristian413@gmail.com>
2024-05-10 20:58:10 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,16 @@ use App\Http\Controllers\backend\ProductManageController;
 use App\Http\Controllers\backend\AdminController;
 
 
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
